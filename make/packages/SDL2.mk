667dc91d60af7ade368bac6ce76eb7353cb250f0
lexouduck <lexou-dit-tuc@hotmail.fr>
2021-11-06 20:48:23 +0100

    fix(Makefile): fixed bad shell call in SDL2.mk package script

--- make/packages/SDL2.mk
+++ make/packages/SDL2.mk
@@ -5,7 +5,7 @@
 LIBSDL = SDL2
 LIBSDL_DIR = $(LIBDIR)$(LIBSDL)/
 LIBSDL_BIN = $(LIBSDL_DIR)bin/$(OSMODE)/
-LIBSDL_VERSION := $(shell cat $(LIBSDL_VERFILE) | cut -d'@' -f 2 | cut -d'-' -f 1)
+LIBSDL_VERSION := $(shell cat $(PACKAGESLIST) | grep '^$(LIBSDL)' | cut -d'@' -f 2 | cut -d'-' -f 1)
 
 LIBSDL_INCLUDE = $(LIBSDL_DIR)include/
 LIBSDL_LINKDIR = $(LIBSDL_BIN)
